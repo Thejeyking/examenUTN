da58e52537523a5918cfb380bbd18b5cb7e39dd7
Matias Oviedo <thejeyking@gmail.com>
2021-03-03 20:12:32 -0300

    Subiendo

--- dos.js
+++ dos.js
@@ -1,4 +1,95 @@
-function mostrar()
-{
-  alert("dos");
+/*
+Realizar el algoritmo que permita ingresar los datos de los alumnos de una division de la UTN FRA, los datos solicitados son:
+nombre
+
+edad (validar)
+se debe informar de existir, o informar que no existe , en el caso que corresponda.
+a) El nombre del mejor promedio que no sea masculino
+b) El nombre del mas joven de los alumnos entre los que la dan libre
+d) El promedio de nota por sexo
+f) La edad y nombre del que cursa mas materias que no sean en forma remota
+*/
+function mostrar() {
+  var nombre;
+  var tipodecursada;
+  var cantidaddematerias;
+  var sexo;
+  var opcsexo;
+  var nota;
+  var edad;
+  nombre = prompt('ingrese su nombre');
+
+  sexo = prompt('Sexo ( femenino , masculino , no binario)');
+  nota = prompt('Nota (entre 0 y 10)');
+  nota = parseInt(nota);
+  edad = prompt('Ingrese la edad');
+  edad = parseInt(edad);
+  var opctipodecursada;
+  do {
+    tipodecursada = prompt('Ingrese un tipo de cursada "libre";"presencial";"remota"');
+    switch (tipodecursada) {
+      case "libre":
+        opctipodecursada = tipodecursada;
+        break;
+      case "presencial":
+        opctipodecursada = tipodecursada;
+        break;
+      case "remota":
+        opctipodecursada = tipodecursada;
+        break;
+      default:
+        alert("Ingrese un tipo de cursada Valida");
+        break;
+    }
+  } while (tipodecursada != opctipodecursada);
+  do {
+    cantidaddematerias = prompt('cantidad de materias( mas de cero y menos de 8)');
+    cantidaddematerias = parseInt(cantidaddematerias);
+  } while (cantidaddematerias < 0 && cantidaddematerias > 8);
+  do {
+    sexo = prompt('Sexo ( femenino , masculino , no binario)');
+    switch (sexo) {
+      case "femenino":
+        opcsexo = sexo;
+        break;
+      case "masculino":
+        opcsexo = sexo;
+        break;
+
+      case "no binario":
+        opcsexo = sexo;
+        break;
+
+      default:
+        alert("Ingrese un sexo valido";)
+        break;
+    }
+  } while (opcsexo != sexo);
+
+
+
+
+
+
+
+
+
+
+  if (nombre == nombregrabado) {
+    if (tipodecursada == tipodecursadagrabado) {
+      if (cantidaddematerias == cantidaddemateriasgrabada) {
+        if (sexo == sexograbado) {
+          if (nota == notagrabada) {
+            if (edad == edadgrabada) {
+              alert("Existe ya este dato");
+            }
+          }
+        }
+      }
+    }
+  } else {
+    alert("Se esta grabando esta informacion")
+  }
+
+
 }
--- uno.js
+++ uno.js
@@ -1,5 +1,182 @@
+/*Debemos realizar la carga de una compra de 5(cinco) productos de desinfección
+de cada una debo obtener los siguientes datos:
+el nombre del producto el tipo de producto (validar "ALCOHOL", "IAC" o "QUAT"),
+el precio (validar entre 100 y 300),
+la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades),
 
-function mostrar()
-{
-	alert("uno");
+Se debe Informar al usuario lo siguiente:
+// El promedio de cantidad por tipo de producto
+// El tipo de inflamable con más cantidad de unidades en total de la compra
+// Cuántas unidades de IAC con precios menos a 200 (inclusive) se compraron en total
+// la marca y tipo del más caro de los productos*/
+function mostrar() {
+	var productos;
+	var opcProductos;
+	var cantidad;
+	var validacionCantidad;
+	var validacionCantidad = false;
+	var tipoDeInflamable;
+	var opcTipodeInflamable;
+	var totalAlcohol;
+	var totalIac;
+	var totalQuat;
+	var cantidadTotal;
+	var cantidadAlcohol;
+	var cantidadIac;
+	var cantidadQuat;
+	var cantidadTotal;
+	var promedioAlcohol;
+	var promedioIac;
+	var promedioQuat;
+	var mayorCantidad;
+	var nombreMayorCantidad;
+	var flag;
+	var masCaro;
+	var precio;
+	var tipodelMascaro;
+	var mardelmascaro;
+	var marca;
+
+	cantidadTotal = 0;
+	totalAlcohol = 0;
+	totalIac = 0;
+	totalQuat = 0;
+	cantidadAlcohol = 0;
+	cantidadIac = 0;
+	cantidadQuat = 0;
+	var ingresos;
+	ingresos = 5;
+	for (var i = 0; i < ingresos; i++) {
+
+		productos = prompt("Ingrese el producto");
+		do {
+			switch (productos) {
+				case "ALCOHOL":
+					opcProductos = productos;
+					cantidadAlcohol = + 1;
+					cantidadTotal = + 1;
+
+					break;
+				case "IAC":
+					opcProductos = productos;
+					cantidadIac = + 1;
+					cantidadTotal = + 1;
+
+					break;
+				case "QUAT":
+					opcProductos = productos;
+					cantidadQuat = + 1;
+					cantidadTotal = + 1;
+					break;
+
+				default:
+					alert("Ingrese un producto valido");
+					productos = prompt("Ingrese el producto");
+					break;
+
+			}
+		} while (productos != opcProductos);
+
+		do {
+			cantidad = prompt("Ingrese la cantidad");
+			cantidad = parseInt(cantidad);
+			if (cantidad < 0 && 1000 > cantidad) {
+				do {
+					switch (productos) {
+						case "ALCOHOL":
+							opcProductos = productos;
+							totalAlcohol = cantidad + totalAlcohol;
+							break;
+						case "IAC":
+							opcProductos = productos;
+							totalIac = cantidad + totalIac;
+
+							break;
+						case "QUAT":
+							opcProductos = productos;
+							totalQuat = cantidad + totalQuat;
+							break;
+
+						default:
+							alert("Ingrese un producto valido");
+							productos = prompt("Ingrese el producto");
+							break;
+
+					}
+				} while (productos != opcProductos);
+				validacionCantidad = true
+			} else {
+				alert("Ingrese una cantidad valida mayor 0 menor a 1000");
+			}
+		} while (validacionCantidad == true);
+		marca = prompt("Ingrese el nombre de la marca");
+		do {
+			tipoDeInflamable = prompt("Ingrese el tipo de inflamable");
+			switch (tipoDeInflamable) {
+				case "ignífugo":
+					opcTipodeInflamable = tipoDeInflamable;
+					break;
+				case "combustible":
+					opcTipodeInflamable = tipoDeInflamable;
+					break;
+				case "explosivo":
+					opcTipodeInflamable = tipoDeInflamable;
+					break;
+
+				default:
+					alert("ngrese el tipo de inflamable Valido");
+					tipoDeInflamable = prompt("Ingrese el tipo de inflamable Valido");
+					break;
+			}
+
+		} while (tipoDeInflamable != opcTipodeInflamable);
+
+		precio = prompt("Ingrese el Precio");
+		precio = parseInt(precio);
+		flag = false
+
+		promedioAlcohol = totalAlcohol % cantidadAlcohol;
+
+		promedioIac = totalIac % cantidadIac;
+
+		promedioQuat = totalQuat % cantidadQuat;
+
+
+		if (totalAlcohol > totalIac) {
+			mayorCantidad = totalAlcohol;
+			nombreMayorCantidad = "alcohol";
+		} else if (totalAlcohol > totalQuat) {
+			mayorCantidad = totalAlcohol;
+			nombreMayorCantidad = "alcohol";
+		} else if (totalIac > totalQuat) {
+			mayorCantidad = totalQuat;
+			nombreMayorCantidad = "Quat";
+		} else {
+			mayorCantidad = totalIac;
+			nombreMayorCantidad = "IAC";
+		}
+
+		// Cuántas unidades de IAC con precios menos a 200 (inclusive) se compraron en total
+		var precioMenorCantIAC;
+		precioMenorCantIAC = 0;
+		if (productos == "IAC") {
+			if (cantidad <= 200) {
+				precioMenorCantIAC = precioMenorCantIAC + cantidad;
+			}
+		}
+
+		// la marca y tipo del más caro de los productos
+
+		if (flag = false) {
+			flag = true
+			mascaro = precio;
+			tipodelMascaro = tipoDeInflamable;
+			mardelmascaro = marca;
+		} else if (precio > mascaro) {
+			masCaro = precio;
+		}
+
+
+	}
+	document.write("El promedio de cantidad por tipo de producto: <Br> alcohol: " + promedioAlcohol + " IAC: " + promedioIac + " QUAT" + promedioQuat + "<br> El tipo de inflamable con más cantidad de unidades en total de la compra: " + nombreMayorCantidad + " " + mayorCantidad + "<br> Cuántas unidades de IAC con precios menos a 200 (inclusive) se compraron en total " + precioMenorCantIAC + "<br> la marca y tipo del más caro de los productos " + mardelmascaro + ", " + tipodelMascaro + ", " + masCaro);
 }
