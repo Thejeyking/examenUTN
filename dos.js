1c0e2e23bd0c8ebbaf4552717319e319c916f6ee
Matias Oviedo <thejeyking@gmail.com>
2021-03-03 20:50:15 -0300

    Hastadondellege

--- dos.js
+++ dos.js
@@ -3,11 +3,7 @@ Realizar el algoritmo que permita ingresar los datos de los alumnos de una divis
 nombre
 
 edad (validar)
-se debe informar de existir, o informar que no existe , en el caso que corresponda.
-a) El nombre del mejor promedio que no sea masculino
-b) El nombre del mas joven de los alumnos entre los que la dan libre
-d) El promedio de nota por sexo
-f) La edad y nombre del que cursa mas materias que no sean en forma remota
+
 */
 function mostrar() {
   var nombre;
@@ -17,10 +13,36 @@ function mostrar() {
   var opcsexo;
   var nota;
   var edad;
+  var flagmejorpromedio;
+  var mejorpromedio
+  flagmejorpromedio = false;
+  var promediodemateriasfemenino;
+  var promediodemateriasmasculino;
+  var promediodemateriasnobinario;
+  var promediodematerias;
+  var promediomasculino;
+  var promediomasculinonota;
+  var promediofemenino;
+  var promediofemeninonota;
+  var promedionobinario;
+  var promedionobinarionota;
+  var promediofinalmasculino;
+  var promediofinalfemenino;
+  var promediofinalnobinario;
+  promediodemateriasfemenino = 0;
+  promediodemateriasmasculino = 0;
+  promediodemateriasnobinario = 0;
+  promediodematerias = 0;
+  
+  var nombremasjoven;
+  var edaddelmasjoven
+  var flagedadmasjoven;
+  flagedadmasjoven = false;
+
+
+
   nombre = prompt('ingrese su nombre');
 
-  sexo = prompt('Sexo ( femenino , masculino , no binario)');
-  nota = prompt('Nota (entre 0 y 10)');
   nota = parseInt(nota);
   edad = prompt('Ingrese la edad');
   edad = parseInt(edad);
@@ -61,27 +83,50 @@ function mostrar() {
         break;
 
       default:
-        alert("Ingrese un sexo valido";)
+        alert("Ingrese un sexo valido");
         break;
     }
   } while (opcsexo != sexo);
+  do {
+    nota = prompt('Nota (entre 0 y 10)');
+  } while (nota < 0 && nota > 11);
 
+  if (sexo != "masculino") {
+    if (flagmejorpromedio == false) {
+      mejorpromedio = nota;
+      flagmejorpromedio = true
+    } else if (mejorpromedio < nota) {
+      mejorpromedio = nota;
+    }
+  }
 
-
-
-
-
-
-
-
-
+  if (tipodecursada == "libre") {
+    if (flagedadmasjoven == false) {
+      edaddelmasjoven = edad;
+      nombremasjoven = nombre;
+      flagedadmasjoven = true;
+    } else if (edad < edaddelmasjoven) {
+      edaddelmasjoven = edad;
+      nombremasjoven = nombre;
+    }
+  }
   if (nombre == nombregrabado) {
     if (tipodecursada == tipodecursadagrabado) {
       if (cantidaddematerias == cantidaddemateriasgrabada) {
         if (sexo == sexograbado) {
           if (nota == notagrabada) {
             if (edad == edadgrabada) {
-              alert("Existe ya este dato");
+              if (sexo == "masculino") {
+                promediodemateriasmasculino = promediodemateriasmasculino + 1;
+                alert("este usuario existe");
+              } else if (sexo == "femenino") {
+                promediodemateriasfemenino = promediodemateriasfemenino + 1;
+                alert("este usuario existe");
+              } else if (sexo == "no binario") {
+                promediodemateriasnobinario = promediodemateriasnobinario + 1;
+                alert("este usuario existe");
+              }
+
             }
           }
         }
@@ -90,6 +135,30 @@ function mostrar() {
   } else {
     alert("Se esta grabando esta informacion")
   }
+  // El promedio de nota por sexo
+
+
+  switch (sexo) {
+    case "masculino":
+      promediomasculinonota = nota;
+      promediomasculino = promediomasculino + nota;
+      break;
+    case "femenino":
+      promediofemeninonota = nota;
+      promediofemenino = promediofemenino + nota;
+      break;
+    case "no binario":
+      promedionobinarionota = nota;
+      promedionobinario = promedionobinario + nota;
+      break;
+  }
+  promediofinalmasculino = promediomasculino % promediodemateriasfemenino;
+  promediofinalfemenino = promediofemenino % promediodemateriasmasculino;
+  promediofinalnobinario = promedionobinario % promediodemateriasnobinario;
+
+  // La edad y nombre del que cursa mas materias que no sean en forma remota
+
+  document.write("El nombre del mejor promedio que no sea masculino" + nombremasjoven + "El nombre del mas joven de los alumnos entre los que la dan libre" + nombremasjoven + "El promedio de nota por sexo" + "masculino: " + promediofinalmasculino + " femenino: " + promediofinalfemenino + "no binario: " + promediofinalnobinario + "La edad y nombre del que cursa mas materias que no sean en forma remota")
 
 
 }
